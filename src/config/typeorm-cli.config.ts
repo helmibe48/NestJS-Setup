835a57b053670f5c45b6e4a74e48473c44e953de
Deathaker <helmibe>
2023-07-09 23:07:22 +0700

    Fix user migration

--- src/config/typeorm-cli.config.ts
+++ src/config/typeorm-cli.config.ts
@@ -2,7 +2,9 @@ import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { Ingredient, Recipe } from 'src/recipe/entity/recipe';
+import { User } from 'src/auth/entity/user';
 import { initialSchema1688896394393 } from 'src/migrations/1688896394393-initial_schema';
+import { addUser1688907886468 } from 'src/migrations/1688907886468-add-user';
 
 config();
 
@@ -16,6 +18,6 @@ export default new DataSource({
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
   logging: configService.get<boolean>('DB_LOGGING'),
-  entities: [Recipe, Ingredient],
-  migrations: [initialSchema1688896394393],
+  entities: [Recipe, Ingredient, User],
+  migrations: [initialSchema1688896394393, addUser1688907886468],
 });
