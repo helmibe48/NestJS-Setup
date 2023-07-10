c9b2ef30db4a3618859235ae2538685b1b80a421
Deathaker <helmibe>
2023-07-10 21:37:44 +0700

    Membuat custom exceptions dan global exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './global-filters/all-exceptions.filter';
+import { HttpExceptionFilter } from './global-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT');
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(
+    new HttpExceptionFilter(configService),
+    new AllExceptionsFilter(httpAdapterHost), //Akan membuat semua exceptions menjadi sama
+  );
+
   await app.listen(port);
 }
 bootstrap();
