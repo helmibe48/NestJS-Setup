91615d7a04864740e73e88d0841c27e9eeec2053
Deathaker <helmibe>
2023-07-11 07:33:36 +0700

    add migration runner and build in production and development

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AllExceptionsFilter } from './global-filters/all-exceptions.filter';
 import { HttpExceptionFilter } from './global-filters/http-exception.filter';
+import { runMigrations } from './migrations/migration-runner';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT');
   const httpAdapterHost = app.get(HttpAdapterHost);
@@ -17,6 +19,7 @@ async function bootstrap() {
     new AllExceptionsFilter(httpAdapterHost), //Akan membuat semua exceptions menjadi sama
   );
 
+  await runMigrations();
   await app.listen(port);
 }
 bootstrap();
